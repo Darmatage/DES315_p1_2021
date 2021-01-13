884b13fb7bbc00613ca4ada5abdc9d80a9fc28ec
FalseArray <73496709+FalseArray@users.noreply.github.com>
2021-01-13 08:50:48 -0800

    Add Scene+Button

---
+++ prototyping1/Assets/Scripts/StudentScripts/JohnPaulAbides/WallSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class WallSwitch : MonoBehaviour
+{
+	public GameObject SwitchOffArt;
+	public GameObject SwitchOnArt;
+
+	public Tilemap WallMap;
+	public List<Tile> WallTiles;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		SwitchOffArt.SetActive(true);
+		SwitchOnArt.SetActive(false);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			SwitchOffArt.SetActive(false);
+			SwitchOnArt.SetActive(true);
+			
+		}
+	}
+
+}

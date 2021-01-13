be5fff6ff1fba445e9480b5299f6dcffa6e95c68
Skyler <cowthepow@gmail.com>
2021-01-12 22:30:16 -0700

    Added Scene and Dashing Player
    Added scene 'SkylerPowers' with object 'SPowers_Player', a player that moves faster and is capable of dashing.

---
+++ prototyping1/Assets/Scripts/StudentScripts/SkylerPowers/SPowers_Dodge.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SPowers_Dodge : MonoBehaviour
+{
+  private Vector3 change; // Player movement direction
+
+  private Transform trans; // Player transform
+  private Rigidbody2D rb2d; // Player ridigbody
+
+  public float dodgeCooldown; // Max cooldown
+  private float cooldownTimer; // Current cooldown
+
+  public float dodgeDistance; // How far go
+
+  public float dodgeDuration; // How long does the dodge last
+  private float dodgeTimer; // How long does the dodge have left
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    gameObject.tag = "Player";
+    cooldownTimer = 0f;
+    trans = gameObject.GetComponent<Transform>();
+    rb2d = gameObject.GetComponent<Rigidbody2D>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (dodgeTimer > 0f)
+    {
+      rb2d.MovePosition(trans.position + (change * dodgeDistance / dodgeDuration) * Time.deltaTime);
+      dodgeTimer -= Time.deltaTime;
+    }
+    else if (cooldownTimer <= 0)
+    {
+      if (Input.GetButtonDown("Fire3"))
+      {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+
+        cooldownTimer = dodgeCooldown;
+        dodgeTimer = dodgeDuration;
+      }
+    }
+    else
+    {
+      cooldownTimer -= Time.deltaTime;
+    }
+  }
+}

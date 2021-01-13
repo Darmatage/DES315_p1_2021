51e580fc1881a244931d8041b0bf3e1eac65f15d
EmptyGraves42 <76964285+EmptyGraves42@users.noreply.github.com>
2021-01-12 20:21:27 -0800

    Attack prefabs
    I added a prefab for a player attack and projectile. Basic functionality of creating a projectile that moves and dies is in but is pretty bare bones at this point.

---
+++ prototyping1/Assets/Scripts/ChaseGravesScripts/ChaseGraves_ProjectileScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseGraves_ProjectileScript : MonoBehaviour
+{
+    public int damage;
+    public float speed;
+    public float life;
+
+    private Vector3 direction;
+    private float timer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction.x = 1.0f;
+        direction.y = 0.0f;
+        direction.z = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + direction * speed;
+
+        timer += Time.deltaTime;
+        if(timer >= life)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

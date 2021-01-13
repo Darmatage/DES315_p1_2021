a2c1d2d190b62d41712427d1896209f34af9965e
Zacary Brown <zacarybrown360@gmail.com>
2021-01-13 04:06:42 -0800

    Shield follows mouse. Enemies with bouncybullets bounce off shield

---
+++ prototyping1/Assets/Scripts/StudentScripts/ZacaryBrown/ZacaryBrown_FollowMouse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZacaryBrown_FollowMouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        this.transform.SetPositionAndRotation(worldPos, Quaternion.identity);
+    }
+}

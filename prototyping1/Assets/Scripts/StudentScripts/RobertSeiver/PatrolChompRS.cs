8e479e7f3c1ef7c0d7cf8d547bb895344979ca21
Robert Seiver <r.seiver@digipen.edu>
2021-01-13 14:37:43 -0800

    Patrol Updated + Adds Player Stun Prefab

---
+++ prototyping1/Assets/Scripts/StudentScripts/RobertSeiver/PatrolChompRS.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PatrolChompRS : MonoBehaviour
+{
+    public GameObject CrunchHandlerPrefab;
+    public GameObject PlayerStunPrefab;
+    
+    public PatrolRS PatrolScript;
+    private GameHandler gameHandlerScript;
+    private bool chomped = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameHandlerScript = GameObject.FindGameObjectWithTag("GameHandler").GetComponent<GameHandler>();
+    }
+    
+    // Chomp the player
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!chomped && other.transform.CompareTag("Player"))
+        {
+            chomped = true;
+            
+            // Play sound effect
+            Instantiate(CrunchHandlerPrefab, transform.position, Quaternion.identity);
+            
+            // Stun player
+            Instantiate(PlayerStunPrefab);
+            
+            // Play animation
+            PatrolScript.Anim.Play("monsterSkull_attack");
+            
+            // Damage player
+            gameHandlerScript.TakeDamage(PatrolScript.ChompDamage);
+            
+            // Destroy object
+            Destroy(transform.parent.gameObject, 0.5f);
+        }
+    }
+}
---
+++ prototyping1/Assets/Scripts/StudentScripts/RobertSeiver/PatrolRS.cs
@@ -0,0 +1,226 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolRS : MonoBehaviour
+{
+	public bool DebugDraw = true; // Display debug drawing
+	[Range(1, 100)] public int ChompDamage = 100;  // Damage done by chomping
+	[Range(1.0f, 10.0f)] public float WalkingSpeed; // Movement speed while patroling
+	[Range(1.0f, 10.0f)] public float ChasingSpeed; // Movement speed while chasing the player
+	[Range(0.1f, 5.0f)] public float CornerningTime = 1.0f; // Time it takes to rotate around corners
+	private float corneringTimer;
+
+	[Range(1.0f, 20.0f)] public float ViewDistance; // Length of vision cone
+	[Range(1.0f, 10.0f)] public float VisionConeWidth; // Width of vision cone
+	public Gradient VisionColorNormal; // Color of the vision cone when normally pathing
+	public Gradient VisionColorSpotted; // Color of the vision cone when the player was spotted
+
+	[Range(1, 10)] public int RaysPerFrame; // The number of vision rays cast per frame
+	public LayerMask VisionLayers; // All layers that are visible to this enemy
+
+	// Aggro switch
+	private bool detectedPlayer = false;
+
+	// Counterclockwise order of nodes along the path this monster will follow
+	public Vector2Int[] Path;
+	private List<Vector3> path;
+	private float PathingEpsilon = 0.01f; // Distance at which a new node is chosen
+	private Vector3 target;
+	private Vector3 oldTarget;
+	private int targetIndex = 0;
+
+	// Map containing nodes for monster pathing nodes
+	public GridLayout NodeMap;
+
+	private LineRenderer visionCone;
+	private Transform player;
+	private Animator anim;
+	public Animator Anim => anim;
+	public SpriteRenderer sensingColor;
+
+	private void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+
+		// Initialize the path
+		path = new List<Vector3>();
+		foreach (Vector2Int node in Path)
+			path.Add(GetNodePosition(node));
+		target = path[0];
+
+		// Initialize cornering timer
+		corneringTimer = CornerningTime;
+
+		// Initialize vision cone
+		visionCone = GetComponent<LineRenderer>();
+		visionCone.colorGradient = VisionColorNormal;
+		
+		// Grab animator component
+		anim = GetComponentInChildren<Animator>();
+	}
+
+	private void Update()
+	{
+		// Don't update if the player is dead
+		if (player == null)
+			return;
+		
+		// Movement logic
+		if (detectedPlayer)
+			ChasePlayer();
+		else
+		{
+			LookForPlayer();
+			Patrol();
+		}
+
+		// Update all vision cone data
+		UpdateVisionCone();
+
+		// Draw all debugging data
+		if (DebugDraw)
+			DrawCurrentPath();
+	}
+
+	#region Behavior
+
+	// Cast out rays to search for the player
+	private void LookForPlayer()
+	{
+		for (int _ = 0; _ < RaysPerFrame; _++)
+		{
+			Vector3 ray = GetRandomVisionRay();
+			if (DebugDraw)
+				DrawRaycast(ray);
+
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, ray, ViewDistance, VisionLayers);
+			if (hit.collider != null && hit.transform.CompareTag("Player"))
+			{
+				detectedPlayer = true;
+				anim.Play("monsterSkull_walk");
+				break;
+			}
+		}
+	}
+	
+	// Cycle through patrol path nodes
+	private void Patrol()
+	{
+		// Turning around a corner
+		if (corneringTimer < CornerningTime)
+		{
+			corneringTimer += Time.deltaTime;
+
+			// Rotate toward new target
+			target = Vector2.Lerp(oldTarget,
+				path[(targetIndex + 1) % Path.Length],
+				corneringTimer / CornerningTime);
+
+			// Update target
+			if (corneringTimer >= CornerningTime)
+			{
+				targetIndex = (targetIndex + 1) % Path.Length;
+				target = path[targetIndex];
+			}
+		}
+
+		// Walking to a node
+		else
+		{
+			// Start rotating toward new target
+			if (Vector3.Distance(target, transform.position) < PathingEpsilon)
+			{
+				corneringTimer = 0.0f;
+				oldTarget = transform.position + (target - transform.position).normalized * ViewDistance;
+			}
+
+			// Move toward target
+			else
+			{
+				target = path[targetIndex];
+				transform.position = Vector2.MoveTowards(transform.position, target, WalkingSpeed * Time.deltaTime);
+			}
+		}
+	}
+
+	// Chase the player
+	private void ChasePlayer()
+	{
+		target = player.transform.position;
+		transform.position = Vector2.MoveTowards(transform.position, target, ChasingSpeed * Time.deltaTime);
+	}
+
+	// Update all vision cone data
+	private void UpdateVisionCone()
+	{
+		float highLerpScale = 0.3f;
+		float lowLerpScale  = 0.05f;
+		
+		// Update cone and ring color depending on if the player was seen
+		if (detectedPlayer)
+		{
+			visionCone.colorGradient = VisionColorSpotted;
+			Color startConeColor = visionCone.startColor;
+			startConeColor.a = highLerpScale * (1.0f + 0.5f * Mathf.Sin(20.0f * Time.time));
+			visionCone.startColor = startConeColor;
+
+			Color ringColor = VisionColorSpotted.colorKeys[0].color;
+			ringColor.a = lowLerpScale * (1.0f + 0.5f * Mathf.Sin(20.0f * Time.time));
+			sensingColor.color = ringColor;
+		}
+		else
+		{
+			visionCone.colorGradient = VisionColorNormal;
+			Color startConeColor = visionCone.startColor;
+			startConeColor.a = highLerpScale * (1.0f + Mathf.Sin(5.0f * Time.time));
+			visionCone.startColor = startConeColor;
+
+			Color ringColor = VisionColorNormal.colorKeys[0].color;
+			ringColor.a = lowLerpScale * (1.0f + Mathf.Sin(5.0f * Time.time));
+			sensingColor.color = ringColor;
+		}
+
+		// Update cone endpoints
+		visionCone.endWidth = VisionConeWidth;
+		visionCone.SetPosition(0, transform.position);
+		visionCone.SetPosition(1, transform.position + (target - transform.position).normalized * ViewDistance);
+	}
+
+	#endregion
+
+	#region Helper Functions
+
+	// Get the world coordinates of the center of the given 2D grid cell
+	private Vector3 GetNodePosition(Vector2Int node)
+	{
+		Vector3 offset = NodeMap.cellSize / 2.0f;
+		offset.z = 0.0f;
+		return NodeMap.CellToWorld(new Vector3Int(node.x, node.y, 0)) + offset;
+	}
+
+	// Returns a random vector in the vision cone of this enemy
+	private Vector3 GetRandomVisionRay()
+	{
+		Vector3 currentPath = (target - transform.position).normalized * ViewDistance;
+		float angle = Mathf.Rad2Deg * Mathf.Atan2(VisionConeWidth / 2.0f, ViewDistance);
+		return Quaternion.AngleAxis(Random.Range(-angle, angle), Vector3.forward) * currentPath;
+	}
+
+	#endregion
+
+	#region Debug
+
+	// Draw a debug line from the enemy to the current target node
+	private void DrawCurrentPath()
+	{
+		Debug.DrawLine(transform.position, target, Color.magenta);
+	}
+
+	// Draw a debug line for the given raycast
+	private void DrawRaycast(Vector3 ray)
+	{
+		Debug.DrawLine(transform.position, transform.position + ray, Color.green, 0.1f);
+	}
+
+	#endregion
+}

f4416bdf83b30836556893f15a051ed8f8b1d036
J Wymer <j.wymer@digipen.edu>
2021-01-13 01:02:04 -0700

    Added Inventory Screen

---
+++ prototyping1/Assets/Scripts/StudentScripts/JWymer/InventoryHandlerJW.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InventoryHandlerJW : MonoBehaviour
+{
+	private GameObject playerObj;
+
+	public static bool InventoryIsOpen = false;
+	public GameObject pauseMenuUI;
+	public KeyCode key = KeyCode.Tab;
+	public bool pauseOnOpen = true;
+
+    void Start()
+	{	
+		if (GameObject.FindGameObjectWithTag ("Player") != null) {
+			playerObj = GameObject.FindGameObjectWithTag ("Player");
+		}
+
+		
+		pauseMenuUI.SetActive(false);
+    }
+		
+	//pause menu
+	void Update (){
+		if (Input.GetKeyDown(key)){
+			if (InventoryIsOpen){ Close(); }
+			else{ Open(); }
+		}
+	}
+
+	void Open(){
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+		InventoryIsOpen = true;
+	}
+
+	public void Close(){
+		pauseMenuUI.SetActive(false);
+		Time.timeScale = 1f;
+		InventoryIsOpen = false;
+	}
+}

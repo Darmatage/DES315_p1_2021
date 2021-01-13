669dca35f51761f8d3ed7bf62b6246684aad30c9
DECADSM <duke129409@gmail.com>
2021-01-13 16:56:02 -0500

    Added personal Lever with script that allows multipls doors

---
+++ prototyping1/Assets/Scripts/StudentScripts/EthanVillasin/LeverFunction.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverFunction : MonoBehaviour
+{
+    public GameObject LeverOn, LeverOff;
+    public GameObject[] Doors;
+    bool active = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeverOn.SetActive(false);
+        LeverOff.SetActive(true);
+        active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (active == false)
+            {
+                LeverOff.SetActive(false);
+                LeverOn.SetActive(true);
+                active = true;
+            }
+            else
+            {
+                LeverOn.SetActive(false);
+                LeverOff.SetActive(true);
+                active = false;
+            }
+        }
+    }
+}

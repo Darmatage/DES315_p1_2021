b181d3014fa068e401f1dff7dc6fc1be5cf7b876
David Dasky <david.dasky@digipen.edu>
2021-01-13 08:53:12 -0500

    Started Rework of Doors and Switches
    Doors can require as many switches as you want to open them. Also edited the main menu button, A10, to go to my scene.

---
+++ prototyping1/Assets/Scripts/StudentDavidDaskyScripts/StudentDoor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace StudentDavidDaskyScripts
+{
+    public class StudentDoor : MonoBehaviour
+    {
+        public GameObject doorClosedArt;
+        public GameObject doorOpenArt;
+        public string nextScene = "MainMenu";
+        [SerializeField]
+        private List<StudentSwitch> switches = new List<StudentSwitch>();
+        
+        private void Start()
+        {
+            doorClosedArt.SetActive(true);
+            doorOpenArt.SetActive(false);
+            gameObject.GetComponent<Collider2D>().enabled = false;
+        }
+
+        private void Update()
+        {
+            if (CheckConditions())
+            {
+                DoorOpen();
+            }
+        }
+
+        private bool CheckConditions()
+        {
+            // if any switch is off return false
+            foreach (var _switch in switches)
+            {
+                if(!_switch.isOn) return false;
+            }
+            return true;
+        }
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(nextScene);
+            }
+        }
+        
+        private void DoorOpen()
+        {
+            // swap art assets 
+            doorClosedArt.SetActive(false);
+            doorOpenArt.SetActive(true);
+            // enable the collider
+            gameObject.GetComponent<Collider2D>().enabled = true;
+        }
+        
+    }
+}
---
+++ prototyping1/Assets/Scripts/StudentDavidDaskyScripts/StudentSwitch.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+namespace StudentDavidDaskyScripts
+{
+    public class StudentSwitch : MonoBehaviour
+    {
+        public GameObject switchOffArt;
+        public GameObject switchOnArt;
+
+        public bool isOn = false;
+        
+        private void Start()
+        {
+            switchOffArt.SetActive(true);
+            switchOnArt.SetActive(false);
+            isOn = false;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.gameObject.CompareTag("Player")) return;
+            switchOffArt.SetActive(false);
+            switchOnArt.SetActive(true);
+            isOn = true;
+        }
+    }   
+}

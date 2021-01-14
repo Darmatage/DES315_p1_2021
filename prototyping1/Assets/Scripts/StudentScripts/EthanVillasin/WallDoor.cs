1d557168d782279bb656cb7680afa1be3fd9b4ed
DECADSM <duke129409@gmail.com>
2021-01-14 01:49:32 -0500

    Added a Wall-Door and linked it to a lever. Lever can Activate Multiple wall-doors

---
+++ prototyping1/Assets/Scripts/StudentScripts/EthanVillasin/WallDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDoor : MonoBehaviour
+{
+    public bool state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenDoor()
+    {
+        if (state == true) //open door
+        {
+            gameObject.SetActive(false);
+            state = false;
+        }
+        else
+        {
+            gameObject.SetActive(true);
+            state = true;
+        }
+    }
+}

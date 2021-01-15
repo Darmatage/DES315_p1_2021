993c782dc5aef8b57595873d96bf170817e0b7d8
Robert Seiver <r.seiver@digipen.edu>
2021-01-14 22:31:38 -0800

    Couple more tweaks

--- prototyping1/Assets/Scripts/StudentScripts/RobertSeiver/PlayerStunRS.cs
+++ prototyping1/Assets/Scripts/StudentScripts/RobertSeiver/PlayerStunRS.cs
@@ -5,7 +5,6 @@ public class PlayerStunRS : MonoBehaviour
     public float StunTime = 1.0f;
     private Rigidbody2D playerRB;
     private Animator playerAnim;
-    private PlayerMoveRS movementScript;
     
     // Start is called before the first frame update
     private void Start()
@@ -13,10 +12,8 @@ public class PlayerStunRS : MonoBehaviour
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         playerRB = player.GetComponent<Rigidbody2D>();
         playerAnim = player.GetComponentInChildren<Animator>();
-        movementScript = player.GetComponent<PlayerMoveRS>();
         Destroy(gameObject, StunTime);
         playerRB.constraints = RigidbodyConstraints2D.FreezeAll;
-        movementScript.CanWalk = false;
     }
 
     private void Update()
@@ -27,6 +24,5 @@ public class PlayerStunRS : MonoBehaviour
     private void OnDestroy()
     {
         playerRB.constraints = RigidbodyConstraints2D.FreezeRotation;
-        movementScript.CanWalk = true;
     }
 }

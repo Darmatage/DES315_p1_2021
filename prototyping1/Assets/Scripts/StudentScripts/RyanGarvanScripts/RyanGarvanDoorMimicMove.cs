49c45f7c102c1731cd0582fcc8d3e6afdda05bfe
Ghostomato <ferrettomato@live.com>
2021-01-18 01:51:10 -0800

    Add animations for door mimic.

--- prototyping1/Assets/Scripts/StudentScripts/RyanGarvanScripts/RyanGarvanDoorMimicMove.cs
+++ prototyping1/Assets/Scripts/StudentScripts/RyanGarvanScripts/RyanGarvanDoorMimicMove.cs
@@ -5,38 +5,50 @@ using UnityEngine;
 // Movement logic script for the Door Mimic "enemy"
 public class RyanGarvanDoorMimicMove : MonoBehaviour
 {
-    SpriteRenderer m_eyeSprite;      // Eye sprite component
-    SpriteRenderer m_pupilSprite;    // Pupil sprite component
-    Vector3 m_pupilInitPos;          // Initial position of the door's pupil relative to the door
-    SpriteRenderer m_outlineSprite;  // Eye outline sprite component
-    Collider2D m_childCollider;      // Collider component of child object (the door itself)
-    RyanGarvanAStarPather m_pather;  // Pathfinding component
-    GameObject m_player;             // Player character
-    bool m_isAwakened = false;       // Whether the door's eye is open yet (opens when approached for the first time)
-    public float m_speed = 20;       // The door's move speed
-    public float m_viewDistance = 5; // How far the door can see
-    public float m_pathRate = 1;
-    float m_pathCountdown = 0;
-    public int m_maxFleeDistance = 10;
-    List<Vector3> m_path;
-    float m_lookAngle = 0; // The angle at which the door is looking
+    Transform m_children;                  // Empty GameObject containing all children
+    SpriteRenderer m_eyeSprite;            // Eye sprite component
+    SpriteRenderer m_pupilSprite;          // Pupil sprite component
+    Sprite m_pupilImageNormal;             // Normal pupil sprite
+    public Sprite m_pupilImageScared;      // Scared pupil sprite
+    Vector3 m_pupilInitPos;                // Initial position of the door's pupil relative to the door
+    SpriteRenderer m_outlineSprite;        // Eye outline sprite component
+    Collider2D m_doorCollider;             // Collider component of child object (the door itself)
+    Vector3 m_doorInitPos;                 // Initial position of the base door relative to the door mimic
+    RyanGarvanAStarPather m_pather;        // Pathfinding component
+    GameObject m_player;                   // Player character
+    bool m_isAwakened = false;             // Whether the door's eye is open yet (opens when approached for the first time)
+    public float m_speed = 20;             // The door's move speed
+    public float m_viewDistance = 10;       // How far the door can see
+    public float m_pathRate = 1;           // The minimum number of seconds between pathfinding checks
+    float m_pathCountdown = 0;             // The number of seconds until the next pathfinding check
+    public int m_maxFleeDistance = 10;     // The maximum distance the door will search to find an escape route
+    List<Vector3> m_path;                  // The path the door is currently following
+    float m_lookAngle = 0;                 // The angle at which the door is looking
+    float m_awarenessDistance = 2.5f;      // The radius within which the door can hear the player
+    float m_searchSpeed = Mathf.PI / 1.5f; // The speed at which the door looks around when it can't see the player
 
     // Start is called before the first frame update
     void Start()
     {
-        m_eyeSprite = GetComponent<SpriteRenderer>(); // Get eye sprite
+        m_children = transform.GetChild(0);
+
+        m_eyeSprite = m_children.GetChild(0).GetComponent<SpriteRenderer>(); // Get eye sprite
         m_eyeSprite.enabled = false;                  // Eye sprite starts hidden
 
-        m_pupilSprite = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        m_pupilSprite = m_children.GetChild(1).GetComponent<SpriteRenderer>();
         m_pupilInitPos = m_pupilSprite.transform.localPosition;
+        m_pupilImageNormal = m_pupilSprite.sprite;
         m_pupilSprite.enabled = false;
 
-        m_outlineSprite = transform.GetChild(1).GetComponent<SpriteRenderer>();
+        m_outlineSprite = m_children.GetChild(2).GetComponent<SpriteRenderer>();
         m_outlineSprite.enabled = false;
 
-        m_childCollider = transform.GetChild(2).GetComponent<Collider2D>();
+        m_doorCollider = m_children.GetChild(3).GetComponent<Collider2D>();
+
+        m_doorInitPos = m_children.localPosition;
 
         m_pather = GetComponent<RyanGarvanAStarPather>();
+        m_path = new List<Vector3>();
 
         m_player = GameObject.FindGameObjectWithTag("Player"); // Get player character
     }
@@ -44,28 +56,38 @@ public class RyanGarvanDoorMimicMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 eyePos = transform.position + new Vector3(0, 1.0f, 0);
-        Vector2 vec_to_player = (Vector2)m_player.transform.position - eyePos; // Vector from door to player
+        m_children.localPosition = m_doorInitPos;
+
+        Vector2 eyePos = transform.position;
+        Vector2 vec_to_player = (Vector2)m_player.transform.position - new Vector2(0, 0.5f) - eyePos; // Vector from door to player
         float dist_from_player = vec_to_player.magnitude;                         // Distance from door to player
 
         List<RaycastHit2D> results = new List<RaycastHit2D>(); // List of raycast hits
         ContactFilter2D filter = new ContactFilter2D();        // Contact filter for raycast
 
         // Perform a raycast to see if the door has a line of sight to the player
-        Physics2D.Raycast(transform.position, vec_to_player.normalized, filter, results, m_viewDistance);
+        Physics2D.Raycast(eyePos + vec_to_player.normalized, vec_to_player.normalized, filter, results, m_viewDistance);
 
         float angleToPlayer = Mathf.Atan2(-vec_to_player.y, vec_to_player.x);
+
+        if (angleToPlayer < 0)
+        {
+            angleToPlayer += Mathf.PI * 2;
+        }
         
+        if (dist_from_player <= m_awarenessDistance)
+        {
+            m_lookAngle = Mathf.Atan2(-vec_to_player.y, vec_to_player.x);
+        }
+
         // If nothing was hit or the player was the first object hit, the door can see the player
         bool can_see_player = (results.Count == 0 || results[0].collider.gameObject == m_player) && Mathf.Abs(Mathf.DeltaAngle(Mathf.Rad2Deg * angleToPlayer, Mathf.Rad2Deg * m_lookAngle) * Mathf.Deg2Rad) < Mathf.PI / 3.0f && dist_from_player <= m_viewDistance;
 
         // If the door is not yet awake, wait for the player to get close
         if (!m_isAwakened)
         {
-            m_lookAngle = Mathf.Atan2(-vec_to_player.y, vec_to_player.x);
-            
             // If the door is open, the player is close, and the door has a line of sight to the player, wake up
-            if (m_childCollider.enabled && dist_from_player <= 2 && can_see_player)
+            if (m_doorCollider.enabled && dist_from_player <= m_awarenessDistance)
             {
                 m_eyeSprite.enabled = true; // Make eye sprite visible
                 m_pupilSprite.enabled = true; // Make pupil sprite visible
@@ -76,7 +98,7 @@ public class RyanGarvanDoorMimicMove : MonoBehaviour
         // If the door is awake, run away from the player if they're close
         else
         {
-            //m_lookAngle = Mathf.Atan2(-vec_to_player.y, vec_to_player.x);
+            m_pupilSprite.sprite = m_pupilImageNormal;
             
             if (m_pathCountdown > 0)
             {
@@ -95,11 +117,8 @@ public class RyanGarvanDoorMimicMove : MonoBehaviour
 
             if (m_path.Count > 0)
             {
-                if (can_see_player)
-                {
-                    m_lookAngle = Mathf.Atan2(-vec_to_player.y, vec_to_player.x);
-                }
-                
+                m_children.localPosition += new Vector3(0, Mathf.Abs(Mathf.Sin(Time.time * 10.0f) / 2.0f), 0);
+
                 Vector3 nextPos = m_path[0];
                 nextPos.z = transform.position.z;
                 Vector3 toNextPos = nextPos - transform.position;
@@ -131,7 +150,20 @@ public class RyanGarvanDoorMimicMove : MonoBehaviour
             }
             else
             {
-                m_lookAngle += Mathf.PI / 2.0f * Time.deltaTime;
+                if (!can_see_player)
+                {
+                    m_lookAngle += m_searchSpeed * Time.deltaTime;
+                }
+                else
+                {
+                    m_children.localPosition += new Vector3(Mathf.Sin(Time.time * 40.0f) / 32.0f, 0, 0);
+                    m_pupilSprite.sprite = m_pupilImageScared;
+                }
+            }
+
+            if (can_see_player)
+            {
+                m_lookAngle = Mathf.Atan2(-vec_to_player.y, vec_to_player.x);
             }
 
             if (m_lookAngle >= Mathf.PI * 2)

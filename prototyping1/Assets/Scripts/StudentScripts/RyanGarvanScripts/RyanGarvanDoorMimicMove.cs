e896c31f9ff6ba2455fd9c1f953ca2a658158c94
Ghostomato <ferrettomato@live.com>
2021-01-17 17:58:33 -0800

    Start implementing field of view for door mimic

--- prototyping1/Assets/Scripts/StudentScripts/RyanGarvanScripts/RyanGarvanDoorMimicMove.cs
+++ prototyping1/Assets/Scripts/StudentScripts/RyanGarvanScripts/RyanGarvanDoorMimicMove.cs
@@ -27,6 +27,13 @@ public class RyanGarvanDoorMimicMove : MonoBehaviour
         m_eyeSprite = GetComponent<SpriteRenderer>(); // Get eye sprite
         m_eyeSprite.enabled = false;                  // Eye sprite starts hidden
 
+        m_pupilSprite = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        m_pupilInitPos = m_pupilSprite.transform.localPosition;
+        m_pupilSprite.enabled = false;
+
+        m_outlineSprite = transform.GetChild(1).GetComponent<SpriteRenderer>();
+        m_outlineSprite.enabled = false;
+
         m_childCollider = transform.GetChild(2).GetComponent<Collider2D>();
 
         m_pather = GetComponent<RyanGarvanAStarPather>();
@@ -37,7 +44,8 @@ public class RyanGarvanDoorMimicMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 vec_to_player = m_player.transform.position - (transform.position + new Vector3(0, 1, 0)); // Vector from door to player
+        Vector2 eyePos = transform.position + new Vector3(0, 1.0f, 0);
+        Vector2 vec_to_player = (Vector2)m_player.transform.position - eyePos; // Vector from door to player
         float dist_from_player = vec_to_player.magnitude;                         // Distance from door to player
 
         List<RaycastHit2D> results = new List<RaycastHit2D>(); // List of raycast hits
@@ -46,26 +54,30 @@ public class RyanGarvanDoorMimicMove : MonoBehaviour
         // Perform a raycast to see if the door has a line of sight to the player
         Physics2D.Raycast(transform.position, vec_to_player.normalized, filter, results, m_viewDistance);
 
+        float angleToPlayer = Mathf.Atan2(-vec_to_player.y, vec_to_player.x);
+        
         // If nothing was hit or the player was the first object hit, the door can see the player
-        bool can_see_player = results.Count == 0 || results[0].collider.gameObject == m_player;
+        bool can_see_player = (results.Count == 0 || results[0].collider.gameObject == m_player) && Mathf.Abs(Mathf.DeltaAngle(Mathf.Rad2Deg * angleToPlayer, Mathf.Rad2Deg * m_lookAngle) * Mathf.Deg2Rad) < Mathf.PI / 3.0f && dist_from_player <= m_viewDistance;
 
         // If the door is not yet awake, wait for the player to get close
         if (!m_isAwakened)
         {
+            m_lookAngle = Mathf.Atan2(-vec_to_player.y, vec_to_player.x);
+            
             // If the door is open, the player is close, and the door has a line of sight to the player, wake up
             if (m_childCollider.enabled && dist_from_player <= 2 && can_see_player)
             {
                 m_eyeSprite.enabled = true; // Make eye sprite visible
+                m_pupilSprite.enabled = true; // Make pupil sprite visible
+                m_outlineSprite.enabled = true; // Make eye outline sprite visible
                 m_isAwakened = true;        // Enable movement
             }
         }
         // If the door is awake, run away from the player if they're close
         else
         {
-            //m_lookAngle += Mathf.PI / 8 * Time.deltaTime;
-            //m_pupilSprite.transform.position
-            // Raycast in the direction of the player to see if they're hiding behind anything
-
+            //m_lookAngle = Mathf.Atan2(-vec_to_player.y, vec_to_player.x);
+            
             if (m_pathCountdown > 0)
             {
                 m_pathCountdown -= Time.deltaTime;
@@ -83,6 +95,11 @@ public class RyanGarvanDoorMimicMove : MonoBehaviour
 
             if (m_path.Count > 0)
             {
+                if (can_see_player)
+                {
+                    m_lookAngle = Mathf.Atan2(-vec_to_player.y, vec_to_player.x);
+                }
+                
                 Vector3 nextPos = m_path[0];
                 nextPos.z = transform.position.z;
                 Vector3 toNextPos = nextPos - transform.position;
@@ -114,8 +131,15 @@ public class RyanGarvanDoorMimicMove : MonoBehaviour
             }
             else
             {
-
+                m_lookAngle += Mathf.PI / 2.0f * Time.deltaTime;
             }
+
+            if (m_lookAngle >= Mathf.PI * 2)
+                m_lookAngle -= Mathf.PI * 2;
+            if (m_lookAngle < 0)
+                m_lookAngle += Mathf.PI * 2;
+
+            m_pupilSprite.transform.localPosition = m_pupilInitPos + new Vector3(Mathf.Cos(m_lookAngle) / 16.0f, -Mathf.Sin(m_lookAngle) / 16.0f, 0);
         }
     }
 }

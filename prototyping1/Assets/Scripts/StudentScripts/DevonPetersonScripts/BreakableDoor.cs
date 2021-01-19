a92159df9d30ea177653ba0d65a292fa2fb2c63a
Devon Peterson <devonmitchell59@gmail.com>
2021-01-19 14:37:40 -0800

    Lots of work on the mechanics
    breakable door added, needs to detect boss tho, crab boss added doesnt do anything tho, switch added for breakable door

---
+++ prototyping1/Assets/Scripts/StudentScripts/DevonPetersonScripts/BreakableDoor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BreakableDoor : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public GameObject DoorClosedArt;
+	public GameObject DoorOpenArt;
+	public GameObject DoorBrokenArt;
+	public string NextScene = "MainMenu";
+
+	public bool open = false;
+	public bool destroyed = false;
+
+	void Start()
+	{
+		DoorClosedArt.SetActive(true);
+		DoorOpenArt.SetActive(false);
+		DoorBrokenArt.SetActive(false);
+		gameObject.GetComponent<Collider2D>().enabled = true;
+	}
+
+	public void BrokenDoorOpen()
+	{
+		DoorClosedArt.SetActive(false);
+		DoorOpenArt.SetActive(true);
+		open = true;
+		//gameObject.GetComponent<Collider2D>().enabled = true;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		
+		if (other.gameObject.tag == "Player")
+		{
+			if (open) 
+			{
+				SceneManager.LoadScene(NextScene);
+			}
+		}
+
+		if (other.gameObject.tag == "CrabWalkBoss")
+		{
+			Debug.Log("collide");
+			DoorBrokenArt.SetActive(true);
+			DoorOpenArt.SetActive(false);
+			DoorClosedArt.SetActive(false);
+			gameObject.GetComponent<Collider2D>().enabled = false;
+			open = false;
+			destroyed = true;
+		}
+	}
+}
---
+++ prototyping1/Assets/Scripts/StudentScripts/DevonPetersonScripts/CrabWalkBoss.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabWalkBoss : MonoBehaviour
+{
+    float speed = 1.0f;
+    float h = 1.0f;
+    float v = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gameObject.transform.position += new Vector3(transform.position.x + (h * speed * Time.deltaTime), transform.position.y + (v * speed * Time.deltaTime), transform.position.z);
+    }
+}

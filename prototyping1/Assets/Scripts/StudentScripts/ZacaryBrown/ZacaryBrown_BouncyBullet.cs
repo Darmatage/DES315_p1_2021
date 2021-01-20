4bdbe9e2bf02377e09b85b3556852fa3deeec171
Zacary Brown <zacarybrown360@gmail.com>
2021-01-20 03:42:53 -0800

    Auto stash before merge of "main" and "origin/main"

--- prototyping1/Assets/Scripts/StudentScripts/ZacaryBrown/ZacaryBrown_BouncyBullet.cs
+++ prototyping1/Assets/Scripts/StudentScripts/ZacaryBrown/ZacaryBrown_BouncyBullet.cs
@@ -5,26 +5,72 @@ using UnityEngine;
 
 public class ZacaryBrown_BouncyBullet : Projectile
 {
+    public int bounces = 2;
+    public bool active = false;
+    private Vector2 velocity;
+    private float projectileTimer = 0.0f;
+
+    private GameHandler gameHandler;
     // Start is called before the first frame update
     void Start()
     {
         transform.position += (GameObject.Find("Player").GetComponent<Transform>().position - transform.position).normalized;
         GetComponent<Rigidbody2D>().velocity = (GameObject.Find("Player").GetComponent<Transform>().position - transform.position).normalized * speed;
+        velocity = (GameObject.Find("Player").GetComponent<Transform>().position - transform.position).normalized *
+                   speed;
+        gameHandler = GameObject.FindGameObjectWithTag ("GameHandler").GetComponent<GameHandler>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //GetComponent<Rigidbody2D>().velocity = vel;
+        projectileTimer += 0.1f; 
+        if (projectileTimer >= projectileLife){
+            Destroy (gameObject);
+        }
     }
 
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        int i = 0;
+        ++i;
+        --i;
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        --bounces;
+
+        if (bounces < 0)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
         Rigidbody2D body = GetComponent<Rigidbody2D>();
         
-        if (other.gameObject.GetComponent<ZacaryBrown_Surface>().Bouncy)
+        if (other.gameObject.GetComponent<ZacaryBrown_Surface>() != null && other.gameObject.GetComponent<ZacaryBrown_Surface>().Bouncy)
         {
-            body.velocity = Vector2.Reflect(body.velocity, other.contacts[0].normal);
+            body.velocity = Vector2.Reflect(velocity, other.contacts[0].normal);
+            velocity = body.velocity;
+        }
+
+        if (other.gameObject.name == "GameObject")
+        {
+            active = true;
+        }
+
+        if (other.gameObject.CompareTag("monsterShooter"))
+        {
+            Destroy(this.gameObject);
+            other.gameObject.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            return;
+        }
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gameHandler.TakeDamage(5);
+            other.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            Destroy(this.gameObject);
         }
     }
 }
---
+++ prototyping1/Assets/Scripts/StudentScripts/ZacaryBrown/ZacaryBrown_Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZacaryBrown_Spawner : MonoBehaviour
+{
+    public int maxSummons = 1;
+    public GameObject Enemy;
+    private List<GameObject> Enemies;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemies = new List<GameObject>(maxSummons);
+
+        for (int i = 0; i < maxSummons; ++i)
+        {
+            Enemies.Add(null);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < maxSummons; ++i)
+        {
+            if (Enemies[i] == null)
+            {
+                Enemies[i] = Instantiate(Enemy, transform.position, Quaternion.identity);
+            }
+        }
+    }
+}

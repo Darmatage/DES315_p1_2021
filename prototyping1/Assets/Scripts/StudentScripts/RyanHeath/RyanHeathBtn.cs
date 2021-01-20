226130d912301736692632bf0f8b754b6c0669ac
Ryan Heath <ryan.heath@digipen.edu>
2021-01-19 18:24:06 -0800

    update button A2

---
+++ prototyping1/Assets/Scripts/StudentScripts/RyanHeath/RyanHeathBtn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace RyanHeath
+{
+    public class RyanHeathBtn : MonoBehaviour
+    {
+        private RectTransform rectTransform;
+        private Vector2 baseSize;
+        private void Awake()
+        {
+            rectTransform = GetComponent<RectTransform>();
+            baseSize = rectTransform.sizeDelta;
+        }
+
+        void Update()
+        {
+            rectTransform.sizeDelta = baseSize + new Vector2(Mathf.Sin(Time.time * 20) * 3, Mathf.Cos(Time.time * 10) * 3);
+        }
+    }
+}

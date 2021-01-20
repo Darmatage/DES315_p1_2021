1dc9099a420ff29a7e4e1df95b783303469ec5b4
Jiwon Jung <jjwon6218@gmail.com>
2021-01-20 05:13:07 -0800

    Added Feedback

--- prototyping1/Assets/Scripts/StudentScripts/JiwonJung/JiwonJung_Proliferation.cs
+++ prototyping1/Assets/Scripts/StudentScripts/JiwonJung/JiwonJung_Proliferation.cs
@@ -8,6 +8,15 @@ public class JiwonJung_Proliferation : MonoBehaviour
     public GameObject monster;
     private float new_x;
 
+    private Renderer rend;
+    public Color col;
+
+    void Start()
+    {
+        rend = GetComponentInChildren<Renderer>();
+        col = rend.material.color;
+    }
+    
     // Update is called once per frame
     void Update()
     {
@@ -19,6 +28,15 @@ public class JiwonJung_Proliferation : MonoBehaviour
             GameObject new_monster = GameObject.Instantiate(monster);
             new_x = transform.position.x - 2.0f;
             new_monster.transform.position = new Vector3(new_x, transform.position.y, transform.position.z);
+        }
+
+        if (time_limit <= 5.0f && time_limit >= 4.5f)
+        {
+            rend.material.SetColor("_Color", Color.blue);
+        }
+        else
+        {
+            rend.material.SetColor("_Color", Color.magenta);
         }
     }
 }

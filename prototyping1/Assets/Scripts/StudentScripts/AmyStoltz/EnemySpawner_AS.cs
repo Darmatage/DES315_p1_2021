b3fc835e2d71f56bcbc13bd2a9a4a7e8231b5cfd
Amy Stoltz <amy.s@digipen.edu>
2021-01-19 22:42:55 -0800

    Made final level.

---
+++ prototyping1/Assets/Scripts/StudentScripts/AmyStoltz/EnemySpawner_AS.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner_AS : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    // Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(spawnEnemy)
+        {
+            if (waitTimer >= 0f)
+            {
+                waitTimer -= Time.deltaTime;
+            }
+            else
+            {
+                Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public static float waitDelay = 2f;
+    float waitTimer = waitDelay;
+
+    bool spawnEnemy = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("ON TRIGGER");
+
+            spawnEnemy = true;
+
+            //if(waitTimer >= 0f)
+            //{
+            //    waitTimer -= Time.deltaTime;
+            //}
+            //else
+            //{
+            //    Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            //    Destroy(gameObject);
+            //}
+        }
+    }
+}

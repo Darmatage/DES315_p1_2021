31497bf88a34f56d628eae207118c5bfc41e6a6f
KeeganTompkins <51837877+KeeganTompkins@users.noreply.github.com>
2021-01-23 12:04:31 -0800

    Level Changes
    -Added mouse prompt.
    -Edited exploding enemy to be better with new exploding enemy code.

---
+++ prototyping1/Assets/DeleteMouseWhenPostUnattached.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteMouseWhenPostUnattached : MonoBehaviour
+{
+    public GameObject mouse;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!GetComponent<KT_Post>().IsAttached)
+        {
+            Destroy(mouse);
+        }
+    }
+}
